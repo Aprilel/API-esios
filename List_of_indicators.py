744ea0e22e8348f4f00566e519d3dc0d2d172d32
Laura Valera <lauravalera06@gmail.com>
2025-08-27 16:31:20 +0200

    Obtain response with date and indicator

--- List_of_indicators.py
+++ List_of_indicators.py
@@ -19,7 +19,20 @@ df_indicators=pd.DataFrame(data['indicators'])
 
 #print(df_indicators['name'],df_indicators['description'],df_indicators['short_name'],df_indicators['id'])
 df_indicators=df_indicators.set_index('id')
-print(df_indicators)
 
 ### Export the information to an excel ###
 df_indicators.to_excel('Indicators.xlsx')
+
+### Add the necessary parameters for indicator search ##
+indicator=input("introduce el id del indicador que quieres consultar y pulsa ENTER: ")
+start_date=input("introduce la fecha inicial en formato YYYY-MM-DDT00:00:00Z y pulsa ENTER: ")
+end_date=input("introduce la fecha final en formato YYYY-MM-DDT00:00:00Z y pulsa ENTER: ")
+
+### Form the complete URL ###
+cURLwithIndicator=cURL+"/"+indicator+"?start_date="+start_date+"&end_date="+end_date
+
+### Obtain the response and export to Excel ###
+response=requests.get(cURLwithIndicator,headers=headers)
+data=response.json()
+df_values=pd.DataFrame(data['indicator']['values'])
+df_values.to_excel('Values_'+indicator+'.xlsx')
